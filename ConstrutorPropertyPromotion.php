7aa1125c10fc8c8483ad32c03969dd2e2afeb5db
miraz66 <mirajulislam0509@gmail.com>
2024-01-13 22:38:25 +0600

    classes

--- ConstrutorPropertyPromotion.php
+++ ConstrutorPropertyPromotion.php
@@ -2,10 +2,20 @@
 
 declare(strict_types= 1);
 class Transaction{
-    public function __construct(private float $amount, private ?string $description = null) {
-        echo $description;
+    public function __construct(
+        private float $amount, 
+        private ?string $description = null
+        ) {}
+
+    public function addTax(float $rate): Transaction {
+        $this->amount += $this->amount * $rate / 100;// calculate the amount for the rate 8, so amount + rate (100 + 8)
+        return $this;
     }
+    public function getAmount(): float { return $this ->amount; }
 }
 
-$transaction = new Transaction(5, "hello");
-var_dump($transaction);
+$amount = (new Transaction(5, "hello"))
+    ->addTax(10)
+    ->getAmount();
+
+var_dump($amount);
