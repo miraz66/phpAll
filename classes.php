0ebc16d6fc5882859c3262114c9750ace08a0b9d
miraz66 <mirajulislam0509@gmail.com>
2024-01-13 11:49:52 +0600

    classes

--- classes.php
+++ classes.php
@@ -2,7 +2,23 @@
 // Classes & objects
 require_once('./test/Transactions.php');
 
+// $transaction = new Transactions(100, "hello world");
+// $transaction->addTax(8);
+// $transaction->applyDiscount(10);
+// var_dump($transaction);
+// var_dump($transaction->getAmount());
 
+// $amount = (new Transactions(100, "hello world"))
+//     ->addTax(8)
+//     ->applyDiscount(10)
+//     ->getAmount();
 
-$transaction = new Transactions();
-var_dump($transaction);
+$amount1 = (new Transactions(100, "hello world"))
+    ->addTax(8)
+    ->applyDiscount(10);
+    
+$amount2 = (new Transactions(100, "hello world"))
+    ->addTax(8)
+    ->applyDiscount(15);
+
+var_dump($amount1->getAmount(), $amount2->getAmount());
--- test/Transactions.php
+++ test/Transactions.php
@@ -1,12 +1,44 @@
 <?php
 
 declare(strict_types=1);
-class Transactions {
-    public ?float $amount;
-    public $description;
+/* class Transactions {
+    private ?float $amount;
+    private $description;
 
     public function __construct(float $amount, string $description) {
         $this->amount = $amount;
         $this->description = $description;
     }
+
+    public function addTax(float $rate) {
+        $this->amount += $this->amount * $rate / 100;// calculate the amount for the rate 8, so amount + rate (100 + 8)
+    }
+
+    public function applyDiscount($rate) {
+        $this->amount -= $this->amount * $rate / 100;
+    }
+
+    public function getAmount(): float { return $this ->amount; }
+} */
+
+class Transactions {
+    private ?float $amount;
+    private $description;
+
+    public function __construct(float $amount, string $description) {
+        $this->amount = $amount;
+        $this->description = $description;
+    }
+
+    public function addTax(float $rate): Transactions {
+        $this->amount += $this->amount * $rate / 100;// calculate the amount for the rate 8, so amount + rate (100 + 8)
+        return $this;
+    }
+
+    public function applyDiscount($rate): Transactions {
+        $this->amount -= $this->amount * $rate / 100;
+        return $this;
+    }
+
+    public function getAmount(): float { return $this ->amount; }
 }
