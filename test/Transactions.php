2d8d8fa5e37c3e1238259861b74b6d68f643dd04
miraz66 <mirajulislam0509@gmail.com>
2024-01-13 11:04:29 +0600

    classes

--- test/Transactions.php
+++ test/Transactions.php
@@ -4,4 +4,9 @@ declare(strict_types=1);
 class Transactions {
     public ?float $amount;
     public $description;
+
+    public function __construct(float $amount, string $description) {
+        $this->amount = $amount;
+        $this->description = $description;
+    }
 }
