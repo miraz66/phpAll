dfd6b4e0b88bf29ac18eeb5504323811555ce808
miraz66 <mirajulislam0509@gmail.com>
2024-01-20 23:48:26 +0600

    Class

--- classes.php
+++ classes.php
@@ -61,5 +61,6 @@ $toaster->addSlice('bread 3');
 $toaster->addSlice('bread 3');
 $toaster->addSlice('bread 3');
 $toaster->addSlice('bread 3');
+$toaster->addSlice('bread 3');
 $toaster->toastBagel();
 $toaster->toast();
