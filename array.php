8a25a46dc490f20e36dc13a98be3fc90610bcd60
miraz66 <mirajulislam0509@gmail.com>
2024-02-15 22:42:21 +0600

    array

--- array.php
+++ array.php
@@ -138,6 +138,7 @@ $array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 $array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 $array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 $array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+$array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 $object = (object) $array;
 $obj = (object) false;
 var_dump($object->{1});
